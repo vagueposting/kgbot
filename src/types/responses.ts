3efcb63c94cbd988d1b28c83c8d751a8219893c7
vagueposting <vagueposted@gmail.com>
2026-08-10 20:57:15 +0800

    FEAT: changed "/add poi" command to "add" subcommand under "/poi"

--- src/types/responses.ts
+++ src/types/responses.ts
@@ -1,10 +1,11 @@
 import { Approaches } from "./approaches";
+import { SkillTags } from "./skilltags";
 
 export interface POIResponse {
   base: string;
   roll_dc?: number;
   approach?: Approaches[];
-  skill_tag?: string[]; // Make an enum of valid skill tags when you get there.
+  skill_tag?: SkillTags[]; // Make an enum of valid skill tags when you get there.
   success?: string;
   failure?: string;
 }
---
+++ src/types/skilltags.ts
@@ -0,0 +1,11 @@
+export type SkillTags =
+  | "homemaking"
+  | "sustaining"
+  | "skulduggery"
+  | "erudition"
+  | "athletics"
+  | "interfacing"
+  | "composure"
+  | "expression"
+  | "aesthetia"
+  | "dominion";
