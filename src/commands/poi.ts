bde80bd8431089d978fe7a760d921c55888a1c8c
vagueposting <vagueposted@gmail.com>
2026-08-10 12:05:51 +0800

    CHORE: somehow i didn't commit the full rename.

---
+++ src/commands/poi.ts
@@ -0,0 +1,54 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
+import { convertToArray } from "./utility/convertToArray";
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("Add PoI")
+    .setDescription(
+      "GM command. Sets a Point of Interest that can be accessed through aliases.",
+    )
+    .addStringOption((option) =>
+      option
+        .setName("PoI Name")
+        .setDescription("Name of the point of interest")
+        .setRequired(true),
+    )
+    .addStringOption((option) =>
+      option
+        .setName("PoI aliases")
+        .setDescription(
+          "List all aliases for the point of interest, separated by commas.",
+        ),
+    )
+    .addStringOption((option) =>
+      option
+        .setName("PoI actions")
+        .setDescription(
+          "List all actions (verbs) that can be done to the point of interest, separated by commas.",
+        ),
+    ),
+  async execute(interaction: ChatInputCommandInteraction) {
+    const poiName = interaction.options.getString("PoI Name");
+    const poiAliases = convertToArray(
+      interaction.options.getString("PoI aliases") ?? "",
+    );
+    const poiActions = convertToArray(
+      interaction.options.getString("PoI actions") ?? "",
+    );
+
+    if (!poiAliases) {
+      await interaction.reply({
+        content:
+          "For player accessibility, it is heavily recommended that Points of Interest have aliases.",
+        ephemeral: true,
+      });
+    }
+
+    if (!poiActions) {
+      await interaction.reply({
+        content:
+          'Without any actions, player characters can only passively "view" the items. Add actions for improved interactability and flavor.',
+      });
+    }
+  },
+};
