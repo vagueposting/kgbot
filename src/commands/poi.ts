dfc04c4984891ecce5b7810088bfd62fc0697faf
vagueposting <vagueposted@gmail.com>
2026-08-12 21:31:10 +0800

    REFACTOR: changed poi.ts to account for groups

--- src/commands/poi.ts
+++ src/commands/poi.ts
@@ -17,9 +17,10 @@ function createPOI(POIname: string, aliases: string[], actions: string[]): POI {
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("poi")
+    .setDescription("GM command group for Points of Interest")
     .addSubcommand((subcommand) =>
       subcommand
-        .setName("create")
+        .setName("create") // CREATE
         .setDescription(
           "GM command. Sets a Point of Interest that can be accessed through aliases.",
         )
@@ -44,43 +45,55 @@ module.exports = {
             ),
         ),
     )
-    .addSubcommandGroup((group) => group.setName("responses")),
+    .addSubcommand((group) =>
+      group
+        .setName("manage") // MANAGE POINTS OF INTEREST
+        .setDescription("Manage POIs as a whole."),
+    )
+    .addSubcommandGroup((group) =>
+      group
+        .setName("responses") // RESPONSE CONTROLS
+        .setDescription("Manage POI response actions"),
+    ),
   async execute(interaction: ChatInputCommandInteraction) {
-    const selectedSubcommand = interaction.options.getSubcommand();
+    const group = interaction.options.getSubcommandGroup(false);
+    const subcommand = interaction.options.getSubcommand();
 
-    switch (selectedSubcommand) {
-      case "create":
-        const poiName = interaction.options.getString("poi_name");
-        const poiAliases = convertToArray(
-          interaction.options.getString("poi_aliases") ?? "",
-        );
-        const poiActions = convertToArray(
-          interaction.options.getString("poi_actions") ?? "",
-        );
-
-        if (!poiAliases) {
-          await interaction.reply({
-            content:
-              "For player accessibility, it is heavily recommended that Points of Interest have aliases.",
-            ephemeral: true,
-          });
-        }
-
-        if (!poiActions) {
-          await interaction.reply({
-            content:
-              'Without any actions, player characters can only passively "view" the items. Add actions for improved interactability and flavor.',
-          });
-        }
-
-        poiActions.forEach((action) => {
-          // Open an 8 min window.
-          // For each action in the list, create a message with a button.
-          // Clicking on the button adds 8 minutes to the timer.
-        });
+    switch (group) {
+      case "manage":
+        // logic for group management
         break;
-
+      case "responses":
+        // logic for response handling
+        break;
       default:
+        switch (subcommand) {
+          case "create":
+            const poiName = interaction.options.getString("poi_name");
+            const poiAliases = convertToArray(
+              interaction.options.getString("poi_aliases") ?? "",
+            );
+            const poiActions = convertToArray(
+              interaction.options.getString("poi_actions") ?? "",
+            );
+
+            if (!poiAliases) {
+              await interaction.reply({
+                content:
+                  "For player accessibility, it is heavily recommended that Points of Interest have aliases.",
+                ephemeral: true,
+              });
+            }
+
+            if (!poiActions) {
+              await interaction.reply({
+                content:
+                  'Without any actions, player characters can only passively "view" the items. Add actions for improved interactability and flavor.',
+              });
+            }
+
+            createPOI(poiName ?? "", poiAliases, poiActions);
+        }
         break;
     }
   },
