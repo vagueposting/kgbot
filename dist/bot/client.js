fa0dd1dd3aa31f5f1dc9742fce92acf3374b0a29
une pile atomique <42937049+vagueposting@users.noreply.github.com>
2026-08-23 02:28:20 +0800

    Delete dist directory

--- dist/bot/client.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createClient = createClient;
-const discord_js_1 = require("discord.js");
-function createClient() {
-    return new discord_js_1.Client({
-        intents: [discord_js_1.GatewayIntentBits.Guilds],
-    });
-}
